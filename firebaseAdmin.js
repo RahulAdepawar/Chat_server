d99886241c9b7ff4d05e9e9c6a674da0773e5708
rxhxl__05 <70502911+RahulAdepawar@users.noreply.github.com>
2026-01-12 11:50:45 +0530

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,6 @@
 
 var admin = require("firebase-admin");
-console.log("process.env.FIREBASE_PROJECT_ID", process.env.FIREBASE_PROJECT_ID)
-console.log("process.env.FIREBASE_CLIENT_EMAIL", process.env.FIREBASE_CLIENT_EMAIL)
-console.log("process.env.FIREBASE_PRIVATE_KEY", process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"))
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
